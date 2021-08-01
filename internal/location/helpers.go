b6233695a38994e32fc2ac76e312db6cdb12e59b
Ben Naylor <nayben@gmail.com>
2021-08-01 14:33:38 +0200

    Signal quality will be another variable

--- internal/location/helpers.go
+++ internal/location/helpers.go
@@ -7,27 +7,20 @@ type GPS_STATUS uint8
 const (
 	WAIT_SKY GPS_STATUS = iota
 	WAIT_FIX
-	FIX_WEAK
-	FIX_GOOD
+	FIX
 )
 
 func CalculateState(sky *gpsd.SKYReport, tpv *gpsd.TPVReport) GPS_STATUS {
 	haveSkyReport := sky != nil
 	have3DFix := tpv.Mode == 3
-	sats := 0
-	if haveSkyReport {
-		sats = len(sky.Satellites)
-	}
 
 	switch {
 	case !haveSkyReport:
 		return WAIT_SKY
 	case haveSkyReport && !have3DFix:
 		return WAIT_FIX
-	case haveSkyReport && have3DFix && sats <= 6:
-		return FIX_WEAK
-	case haveSkyReport && have3DFix && sats > 6:
-		return FIX_GOOD
+	case haveSkyReport && have3DFix:
+		return FIX
 	default:
 		return 255
 	}
