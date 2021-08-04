54eb02a98afde2a9e51a5ddb9cd4764d893df4dc
Ben Naylor <nayben@gmail.com>
2021-08-04 21:17:07 +0200

    Async persistance for slow database is nice

--- internal/persistence/persistence.go
+++ internal/persistence/persistence.go
@@ -1,6 +1,9 @@
 package persistence
 
 import (
+	"context"
+
+	"github.com/b-n/delicious-gps/internal/logging"
 	"gorm.io/gorm"
 )
 
@@ -13,4 +16,20 @@ func Open(database gorm.Dialector) (*gorm.DB, error) {
 
 	db.AutoMigrate(&PositionData{})
 	return db, nil
+}
+
+func Listen(ctx context.Context, done chan bool, db *gorm.DB, data chan interface{}) {
+	go func() {
+		for {
+			select {
+			case d := <-data:
+				if result := db.Create(d); result.Error != nil {
+					logging.Infof("Failed to save db record")
+				}
+			case <-ctx.Done():
+				done <- true
+				return
+			}
+		}
+	}()
 }
