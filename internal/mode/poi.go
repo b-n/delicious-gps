13d02c2a2d76296ce9b8f301e67bb2c1753a3257
Ben Naylor <nayben@gmail.com>
2021-08-05 22:26:45 +0200

    Store the POI, check the time, push the data, success

--- internal/mode/poi.go
+++ internal/mode/poi.go
@@ -1,11 +1,16 @@
 package mode
 
 import (
+	"time"
+
 	"github.com/b-n/delicious-gps/internal/gpio"
 	"github.com/b-n/delicious-gps/simple_button"
 )
 
-type PoiMode struct{}
+type PoiMode struct {
+	lastLocationEvent interface{}
+	lastEvent         time.Time
+}
 
 func NewPoiMode() ModeHandler {
 	pm := PoiMode{}
@@ -13,10 +18,15 @@ func NewPoiMode() ModeHandler {
 }
 
 func (p *PoiMode) HandleInput(e simple_button.EventPayload) *gpio.OutputPayload {
-	//log POI Datapoint
-	return &gpio.OutputPayload{false, uint32(0xff0000)}
+	if p.lastEvent.Before(time.Now().Add(-5 * time.Second)) {
+		return &gpio.OutputPayload{false, uint32(0xff0000)}
+	}
+
+	writeDataChannel(p.lastLocationEvent, POI, 0)
+	return &gpio.OutputPayload{false, uint32(0xffffff)}
 }
 
 func (p *PoiMode) HandleLocationEvent(e interface{}) {
-	// store in cache
+	p.lastLocationEvent = e
+	p.lastEvent = time.Now()
 }
